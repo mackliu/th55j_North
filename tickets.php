777d5487981602537847fe761df995e0b36b605b
mackliu <macklun@gmail.com>
2025-02-21 15:14:49 +0800

    歷年相同內容調整

--- tickets.php
+++ tickets.php
@@ -6,8 +6,8 @@
     echo $_POST["verify"]; // 使用者輸入的驗證碼
     echo $_POST["verify_ans"]; // 正確驗證碼
     // mysql:host=主機位置、dbname 資料庫名稱、charset 編碼
-    $dsn = "mysql:host=localhost;dbname=web05;charset=utf8";
-    $pdo = new PDO($dsn,"admin","1234");
+    $dsn = "mysql:host=localhost;dbname=th55_north;charset=utf8";
+    $pdo = new PDO($dsn,"root","");
 
     // $sql = "select * from `tickets`"; // sql 查詢語句
     // $row = $pdo->query($sql)->fetchAll();
