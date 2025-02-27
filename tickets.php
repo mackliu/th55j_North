3e597b923e6cdc9939684333947084697ebaa3b1
mackliu <macklun@gmail.comm>
2025-02-27 21:31:22 +0800

    簡化拖拉程式內容,增加註解

--- tickets.php
+++ tickets.php
@@ -6,7 +6,7 @@
     echo $_POST["verify"]; // 使用者輸入的驗證碼
     echo $_POST["verify_ans"]; // 正確驗證碼
     // mysql:host=主機位置、dbname 資料庫名稱、charset 編碼
-    $dsn = "mysql:host=localhost;dbname=th55_north;charset=utf8";
+    $dsn = "mysql:host=localhost;dbname=th55j_north;charset=utf8";
     $pdo = new PDO($dsn,"root","");
 
     // $sql = "select * from `tickets`"; // sql 查詢語句
